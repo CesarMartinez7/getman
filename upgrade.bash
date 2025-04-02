a152379ff939d8241a4e438efec65a0a0efa8844
Cesar Martinez <cesarwamartinez@gmail.com>
2025-04-02 11:23:56 -0500

    probando el upgrade bash

--- upgrade.bash
+++ upgrade.bash
@@ -8,6 +8,11 @@ read -p "Nombre Rama: " rama
 # Leer el mensaje del commit
 read -p "Comment_commit: " comment_commit
 
+
+concurrent_rama = $("git branch --show-current")
+
+
+
 # Comandos Git
 git add .
 git commit -m "$comment_commit"
