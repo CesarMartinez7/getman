ef34c0a7a1041ac382b987ec1bd13d3b2ec87e4e
Cesar Martinez <cesarwamartinez@gmail.com>
2025-04-02 12:11:28 -0500

    Hello en upgrade bash

---
+++ testing.sh
@@ -0,0 +1,7 @@
+
+number=10
+
+if [[ $number == 10 ]]
+then
+    echo "Es igual"
+fi
--- upgrade.bash
+++ upgrade.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
-# Asignacion de los colores para la terminal de bash
 
+# Asignacion de los colores para la terminal de bash
 RED="\e[31m"
 GREEN="\e[32m"
 ENDCOLOR="\e[0m"
@@ -8,22 +8,35 @@ ENDCOLOR="\e[0m"
 
 echo "Generando push"
 
+# Rama actual para Push
+concurrent_rama=$(git branch --show-current)
 
 # Leer el mensaje del commit
 read -p "Comentario Commit: " comment_commit
 
-# Ejecucion del comando entre por asi decirlo tuplas para ejecutar el comando y guardarlo en una variable
-concurrent_rama=$(git branch --show-current)
+# Para ejecutar los condicionales en bash las sintaxis es muy importante para generar esto 
+
+# Para saber la longitud de los caracteres de una variable se pone primero el colchon en la parte de la variable para saber el numero de caracteres o logitud q tiene una variable o cadena de texto
+
+if [[ ${#comment_commit} == 0 ]]
+then
+    echo Asegurate de poner texto en tu Commit
+else
+    # Comandos Git
+    # Utilizamos el caret para hacer que nuestra variable al menos en esa parte este en mayuscula, si queremos que se la primera letra entonces la , al final de la variable y si quermemos que todas sean minusculas entonces las cambiamos a una sola caret ^
+    echo -e "${GREEN}Rama a la Ejecutar push ${concurrent_rama^^}, ${ENDCOLOR}"
+    git add .
+    git commit -m "$comment_commit"
+    # Ejecucion del comando entre por asi decirlo tuplas para ejecutar el comando y guardarlo en una variable
+    resultado=$(git push origin "$cocurrent_rama")
+    echo $resultado
+    
+fi
 
 
-# Utilizamos el caret para hacer que nuestra variable al menos en esa parte este en mayuscula, si queremos que se la primera letra entonces la , al final de la variable y si quermemos que todas sean minusculas entonces las cambiamos a una sola caret ^
-
-echo -e "${GREEN}Rama a la Ejecutar push ${concurrent_rama^^}, ${ENDCOLOR}"
 
 
-# Comandos Git
-git add .
-git commit -m "$comment_commit"
-resultado=$(git push origin "$cocurrent_rama")
 
-echo $resultado
+
+
+
